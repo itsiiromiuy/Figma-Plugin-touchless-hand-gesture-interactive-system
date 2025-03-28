dea9c61a93f5464d101518539b1f529782cf79d4
itsiiromiuy ♥ <109933604+itsiiromiuy@users.noreply.github.com>
2025-03-28 01:05:41 -0500

    register thumbup

--- src/code.ts
+++ src/code.ts
@@ -1,17 +1,37 @@
 import ThumbUp from "./images/thumb_up.png";
 figma.showUI(__html__, { width: 300, height: 200 });
+
+type Gesture = {
+  name: string;
+  score: number;
+};
+type ReceivedData = {
+  gestures: Gesture[];
+  width: number;
+  height: number;
+};
+
 figma.ui.onmessage = (message) => {
   if (message.type === "WS_DATA") {
-    const receivedData = message.payload;
-    console.log("Data from WebSocket:", receivedData);
-    receivedData.forEach((data: any) => {
-      console.log("Data:", data);
-      figma.notify("Received data: " + JSON.stringify(data));
-    });
+    const receivedData: ReceivedData[] = JSON.parse(message.payload);
+    if (!Array.isArray(receivedData)) {
+      console.error("❌ Payload is not an array:", receivedData);
+      return;
+    }
+    // [{"name":"U_SIGN","score":9.555555555555557}]}]
+
+    const firstMatchThumbsUp = receivedData.find(
+      (data) =>
+        Array.isArray(data.gestures) &&
+        data.gestures.some((gesture) => gesture.name === "thumbs_up")
+    );
+
+    if (firstMatchThumbsUp) {
+      showThumbUpImage();
+    }
   } else if (message.type === "WS_ERROR") {
     figma.notify("WebSocket error: " + message.payload);
   } else if (message.type === "WS_OPEN") {
-    showImage();
     console.log("WebSocket connection opened in UI");
     figma.notify("Connected to WebSocket!");
   } else if (message.type === "WS_CLOSE") {
@@ -25,7 +45,7 @@ figma.ui.onmessage = (message) => {
 
 // figma.createImage(new Uint8Array(0)); // Dummy
 // TODO add time out to delete image
-function showImage() {
+function showThumbUpImage() {
   figma.createImageAsync(ThumbUp).then(async (image: Image) => {
     // Create a rectangle that's the same dimensions as the image.
     const node = figma.createRectangle();
