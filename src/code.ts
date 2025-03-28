92be8aee751f9bc011dd1cace668e4c064d3c43c
itsiiromiuy ♥ <109933604+itsiiromiuy@users.noreply.github.com>
2025-03-28 18:20:04 -0500

    adding more gesture

--- src/code.ts
+++ src/code.ts
@@ -1,5 +1,11 @@
 import ThumbUp from "./images/thumb_up.png";
+import Victory from "./images/victory.png";
+import Support from "./images/support.png"
+import Ok from "./images/ok.png"
 figma.showUI(__html__, { width: 300, height: 200 });
+
+let emojiElement: RectangleNode | null = null;
+let emojiName: string = "";
 
 type Gesture = {
   name: string;
@@ -11,6 +17,11 @@ type ReceivedData = {
   height: number;
 };
 
+const THUMBS_UP_SIGN = "thumbs_up";
+const VICTORY_SIGN = "U_SIGN";
+const OK_SIGN = "OK_SIGN";
+const FULL_HAND_UP_SIGN = "FULL_HAND_UP";
+
 figma.ui.onmessage = (message) => {
   if (message.type === "WS_DATA") {
     const receivedData: ReceivedData[] = JSON.parse(message.payload);
@@ -20,14 +31,34 @@ figma.ui.onmessage = (message) => {
     }
     // [{"name":"U_SIGN","score":9.555555555555557}]}]
 
-    const firstMatchThumbsUp = receivedData.find(
-      (data) =>
-        Array.isArray(data.gestures) &&
-        data.gestures.some((gesture) => gesture.name === "thumbs_up")
-    );
+    const matchedData = receivedData.find((data) => {
+      return data.gestures.find((gesture) => gesture.score > 9.0);
+    });
 
-    if (firstMatchThumbsUp) {
-      showThumbUpImage();
+    const gestureType = matchedData?.gestures[0].name;
+    switch (gestureType) {
+      case THUMBS_UP_SIGN:
+        if (matchedData && emojiName !== THUMBS_UP_SIGN) {
+          showThumbUpImage();
+        }
+        break;
+      case VICTORY_SIGN:
+        if (matchedData && emojiName !== VICTORY_SIGN) {
+          showVictoryImage();
+        }
+        break;
+      case OK_SIGN:
+        if (matchedData && emojiName !== OK_SIGN) {
+          showOkImage();
+        }
+        break;
+      case FULL_HAND_UP_SIGN:
+        if (matchedData && emojiName !== FULL_HAND_UP_SIGN) {
+          showHandUpImage();
+        }
+        break;
+      default:
+        console.log(`Sorry, we are out of ${gestureType}.`);
     }
   } else if (message.type === "WS_ERROR") {
     figma.notify("WebSocket error: " + message.payload);
@@ -43,7 +74,30 @@ figma.ui.onmessage = (message) => {
   }
 };
 
-// figma.createImage(new Uint8Array(0)); // Dummy
+function showHandUpImage() {
+  figma.createImageAsync(Support).then(async (image: Image) => {
+    // Create a rectangle that's the same dimensions as the image.
+    const node = figma.createRectangle();
+
+    const { width, height } = await image.getSizeAsync();
+    node.resize(width, height);
+
+    // Render the image by filling the rectangle.
+    node.fills = [
+      {
+        type: "IMAGE",
+        imageHash: image.hash,
+        scaleMode: "FILL",
+      },
+    ];
+    if (emojiElement !== null) {
+      emojiElement.remove();
+    }
+    emojiElement = node;
+    emojiName = FULL_HAND_UP_SIGN;
+  });
+}
+
 // TODO add time out to delete image
 function showThumbUpImage() {
   figma.createImageAsync(ThumbUp).then(async (image: Image) => {
@@ -61,7 +115,58 @@ function showThumbUpImage() {
         scaleMode: "FILL",
       },
     ];
+    if (emojiElement !== null) {
+      emojiElement.remove();
+    }
+    emojiElement = node;
+    emojiName = THUMBS_UP_SIGN;
   });
 }
 
-//
+function showVictoryImage() {
+  figma.createImageAsync(Victory).then(async (image: Image) => {
+    // Create a rectangle that's the same dimensions as the image.
+    const node = figma.createRectangle();
+
+    const { width, height } = await image.getSizeAsync();
+    node.resize(width, height);
+
+    // Render the image by filling the rectangle.
+    node.fills = [
+      {
+        type: "IMAGE",
+        imageHash: image.hash,
+        scaleMode: "FILL",
+      },
+    ];
+    if (emojiElement !== null) {
+      emojiElement.remove();
+    }
+    emojiElement = node;
+    emojiName = VICTORY_SIGN;
+  });
+}
+
+function showOkImage() {
+  figma.createImageAsync(Ok).then(async (image: Image) => {
+    // Create a rectangle that's the same dimensions as the image.
+    const node = figma.createRectangle();
+
+    const { width, height } = await image.getSizeAsync();
+    node.resize(width, height);
+
+    // Render the image by filling the rectangle.
+    node.fills = [
+      {
+        type: "IMAGE",
+        imageHash: image.hash,
+        scaleMode: "FILL",
+      },
+    ];
+    if (emojiElement !== null) {
+      emojiElement.remove();
+    }
+    emojiElement = node;
+    emojiName = OK_SIGN;
+  });
+}
